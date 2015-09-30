9ec19c1961cb5109ae9e9800b2fecaff2f265771
wp132316 <shindirn@naver.com>
2015-09-30 12:34:02 +0900

    .

---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *
+ * <pre>
+ * org.dimigo.collection
+ *      |_ Music
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 9. 25.
+ * </pre>
+ * @User            : nangho
+ * @author         : 신창호
+ * @version         : 1.0
+ */
+public class Music {
+	
+	private String title;
+	private String singer;
+	
+	public Music() {}
+	public Music(String title, String singer) {
+		this.title = title;
+		this.singer = singer;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getSinger() {
+		return singer;
+	}
+	
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	@Override
+	public String toString() {
+		return "Music [title=" + title + ", singer=" + singer + "]";
+	}
+	
+	
+
+}
